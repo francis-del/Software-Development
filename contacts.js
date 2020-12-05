6cb5deef5bcc932beada47eb685bd90a5ba6a0ab
Adindu Maduekwe (maduekwea) <maduekwea@coventry.ac.uk>
2020-12-05 18:33:54 +0000

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,4 +1,5 @@
 
+
 /** @module Contacts*/
 
 import sqlite from 'sqlite-async'
@@ -9,43 +10,49 @@ import sqlite from 'sqlite-async'
  */
 
 class Contacts {
-  /**
+ /**
    * Create an account object
    * @param {String} [dbNmae=":memory:"] - The name of the database file to use.
-   */
-  
-  constructor(dbName = 'memory:') {
-    return (async() => {
-      this.db = await sqlite.open(dbName)
-      // we need this table to store the user accounts
-      const sql = 'CREATE TABLE IF NOT EXISTS contacts(\
-        id INTEGER PRIMARY KEY AUTOINCREMENT,\
-        userid INTEGER,\
-        firstname TEXT NOT NULL,\
-        lastname TEXT NOT NULL,\
-        cause TEXT NOT NULL,\
-        lastcontact INTEGER,\
-        FOREIGN KEY(userid) REFERENCES users(id)\
+ */
+ constructor(dbName = 'memory:') {
+  return (async() => {
+   this.db = await sqlite.open(dbName)
+   // we need this table to store the user accounts
+  const sql = 'CREATE TABLE IF NOT EXISTS contacts(\
+    id INTEGER PRIMARY KEY AUTOINCREMENT,\
+    userid INTEGER,\
+    firstname TEXT NOT NULL,\
+    lastname TEXT NOT NULL,\
+    cause TEXT NOT NULL,\
+    lastcontact INTEGER,\
+    FOREIGN KEY(userid) REFERENCES users(id)\
       );'
-      await this.db.run(sql)
-      return this
-    })()
+   await this.db.run(sql)
+   return this
+  })()
+  }
+ /**
+  * retrives all the contacts in the system
+  * @returns {Array} returns an array containing all the contacts in the database
+ */
+ async all() {
+  const sql = 'SELECT users.user, contacts.* FROM contacts, users\
+                  WHERE contacts.userid = users.id;'
+  const contacts = await this.db.all(sql)
+  for(const index in contacts) {
+   if(contacts[index].photo === null) contacts[index].photo = 'avatar.png'
+  }
+  return contacts
+ }
+  
+  async add(data) {
+    console.log('ADD')
+    console.log(data)
+    return true
   }
   
-  /**
-   * retrives all the contacts in the system
-   * @returns {Array} returns an array containing all the contacts in the database
-   */
-  
-  async all() {
-    const sql = 'SELECT users.user, contacts.* FROM contacts, users\
-                  WHERE contacts.userid = users.id;'
-    const contacts = await this.db.all(sql)
-    for(const index in contacts) {
-      if(contacts[index].photo === null) contacts[index].photo = 'avatar.png'
-    }
-    return contacts
+  async close() {
+    await this.db.close()
   }
 }
-
 export default Contacts
