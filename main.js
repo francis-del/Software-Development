7fcf292043735f4b517e568fb7b721c93a48d443
Adindu Maduekwe (maduekwea) <maduekwea@coventry.ac.uk>
2020-12-08 19:08:34 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 /* main.js */
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
 	console.log('DOMContentLoaded')
-  const delay = 3000
+  const delay = 2000
   document.querySelector('aside').hidden = false
   window.setTimeout( () => {
     document.querySelector('aside').hidden = true
