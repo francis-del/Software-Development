9cc3ef70f4e5a8ac35d49953521ab328413a94ff
Adindu Maduekwe (maduekwea) <maduekwea@coventry.ac.uk>
2020-12-08 19:09:36 +0000

    Update accounts.sql

--- accounts.sql
+++ accounts.sql
@@ -1,31 +1,25 @@
 
-
 --data needed to implement the home screen
 CREATE TABLE IF NOT EXISTS contacts(
  id INTEGER PRIMARY KEY AUTOINCREMENT,
   userid INTEGER,
+  cause TEXT,
+  photo TEXT,
   firstname TEXT NOT NULL,
-  lastname TEXT NOT NULL,
-  cause TEXT NOT NULL,
+  lastname TEXT NOT NULL, 
+  fund TEXT NOT NULL,
+  pledge TEXT NOT NULL,
   url TEXT,
-  image TEXT,
-  fund INTEGER UNSIGNED,
-  pledge INTEGER  UNSIGNED,
   deadline DATE,
-  lastcontact INTEGER,
   FOREIGN KEY(userid) REFERENCES users(id)
   
 );
 
-INSERT INTO contacts(userid, firstname, lastname, cause, url, image, fund, pledge, deadline, lastcontact)
- VALUES(1, "Mark", "Randy", "Coronavirus", "https://cutewallpaper.org/download.php?file=/21/thumbnail-background/Best-50-Background-YouTube-on-HipWallpaper-YouTube-.jpg", "image_1.jpg", "£500", "£50", "12/11/2020", CURRENT_TIMESTAMP);
-INSERT INTO contacts(userid, firstname, lastname, cause, url, image, fund, pledge, deadline, lastcontact)
- VALUES(2, "Chris", "Smalling", "Coronavirus", "https://www.health.org.uk/sites/default/files/styles/landscape_3_2/public/2020-06/20200609_Responding%20to%20COVID-19_collection%20thumbnail.png?itok=YRRIJB7y", "image_2.jpg", "£200", "£100", "10/10/2020", CURRENT_TIMESTAMP);
-INSERT INTO contacts(userid, firstname, lastname, cause, image, fund, pledge, deadline, lastcontact)
-  VALUES(1, "Steve", "Jobs", "Disaster", "image_3.jpg", "£1000", "£500", "10/12/2020", CURRENT_TIMESTAMP);
-INSERT INTO contacts(userid, firstname, lastname, cause, image, fund, pledge, deadline, lastcontact)
-  VALUES(3, "John", "Doe", "Disaster", "image_3", "£2000", "£200", "10/06/2020", CURRENT_TIMESTAMP);
-  INSERT INTO contacts(userid, firstname, lastname, cause, fund, deadline, lastcontact)
-   VALUES(2, "John", "King", "Ebola", "£500", "2/01/2021", CURRENT_TIMESTAMP);
- INSERT INTO contacts(userid, firstname, lastname, cause, pledge, lastcontact)
-    VALUES(4, "Dan", "Smith", "Ebola", "£200", CURRENT_TIMESTAMP);
+INSERT INTO contacts(userid, firstname, lastname, cause, photo, url, fund, pledge, deadline)
+ VALUES(1, "Mark", "Randy", "Coronavirus", "avatar.png", "https://www.unison.org.uk/content/uploads/2020/03/corona-4881364_1920-745x420.jpg", "£2000", "£200", "23/06/2020");
+INSERT INTO contacts(userid, firstname, lastname, cause, photo, url, fund, pledge, deadline)
+ VALUES(1,"Chris", "Smalling", "Disaster", "avatar.png", "https://www.infoplease.com/sites/infoplease.com/files/inline-images/Tohoku%20Aftermath.jpg", "£500", "£100", "25/07/2020");
+INSERT INTO contacts(userid, firstname, lastname, photo, url, fund, pledge, deadline, cause)
+  VALUES(1, "Steve", "Jobs", "avatar.png", "https://www.infoplease.com/sites/infoplease.com/files/inline-images/Tohoku%20Aftermath.jpg", "£500", "£100", "30/8/2020", "Disaster");
+INSERT INTO contacts(userid, firstname, lastname, url, fund, cause, pledge, deadline)
+  VALUES(1,"John", "Doe", "https://medlineplus.gov/ency/images/ency/fullsize/17160.jpg", "£400", "Ebola", "£50", "23/11/2020");
