d0f5462f9b90ac29805adfe22da02b6ae077927e
Adindu Maduekwe (maduekwea) <maduekwea@coventry.ac.uk>
2020-12-11 17:18:13 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+
+/* main.js */
+
+window.addEventListener('DOMContentLoaded', () => {
+	console.log('DOMContentLoaded')
+  const delay = 2000
+  document.querySelector('aside').hidden = false
+  window.setTimeout( () => {
+    document.querySelector('aside').hidden = true
+    }, delay)
+  })
+
+ if(document.querySelector('button.back')) {
+  document.querySelectorAll('button.back').forEach( element => {
+   element.addEventListener('click',() => {
+    console.log('back button clicked')
+    console.log(window.history)
+    window.history.back()
+   })
+  })
+ }
